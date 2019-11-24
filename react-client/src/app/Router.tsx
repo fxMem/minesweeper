c205fec4a4337ee6282e755030d49cc3fb18bb53
Paul <faarseer@ya.ru>
2019-11-24 21:46:04 +0300

    added lobbyComponent

--- react-client/src/app/Router.tsx
+++ react-client/src/app/Router.tsx
@@ -2,14 +2,17 @@ import * as React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Route, Switch, Redirect } from "react-router";
 import { Client } from "../client/Client";
+import { LobbyComponent } from "../lobby/LobbyComponent";
 
 export function Router({ client }: { client: Client }) {
     return (
         <BrowserRouter>
             <Switch>
-                <Redirect exact={true} from='/' to='/session' />
+                <Redirect exact={true} from='/' to='/lobby' />
 
-                <Route path="/session" > </Route>
+                <Route path="/lobby">
+                    <LobbyComponent sessionClient={client}></LobbyComponent>
+                </Route>
             </Switch>
         </BrowserRouter>)
 }
---
+++ react-client/src/lobby/LobbyComponent.tsx
@@ -0,0 +1,81 @@
+import * as React from "react";
+import { Button } from "../common/Button";
+import { useState } from "react";
+import { SessionClient } from "../client/SessionClient";
+import { SessionInfo } from "seedengine.client/session/SessionInfo";
+import { useRouteMatch, useHistory, useLocation } from "react-router-dom";
+
+const lightGreen = '#70d48b';
+
+export function LobbyComponent({ sessionClient }: { sessionClient: SessionClient }) {
+
+    const history = useHistory();
+    const [joiningError, setJoiningError] = useState('');
+    const [newSessionDescription, setNewSessionDescription] = useState('');
+    const [activeSesions, setActiveSessions] = useState([] as SessionInfo[]);
+    const [creationFormVisible, setCreationFormVisible] = useState(false);
+
+
+    function toggleCreatePanelVisibility() {
+        setCreationFormVisible((prev) => !prev);
+    }
+
+    async function updateActiveSessions() {
+        let sessions = await sessionClient.getSessions();
+        setActiveSessions(sessions);
+    }
+
+    async function join(sessionId: string) {
+        const { success, message } = await sessionClient.joinSession(sessionId);
+        if (success) {
+            history.push('/room', { sessionId });
+        }
+        else {
+            setJoiningError(message);
+        }
+    }
+
+    async function createNew() {
+        await sessionClient.createSession(false, newSessionDescription, false);
+        updateActiveSessions();
+    }
+
+    return <>
+        <div style={{ display: 'flex', backgroundColor: lightGreen, padding: '1em', alignItems: 'center' }}>
+            <span style={{ marginRight: '1em' }}>List of active sessions</span>
+            <Button onClick={updateActiveSessions}>Update</Button>
+        </div>
+
+        {
+            activeSesions && <div>
+                {
+                    activeSesions.map
+                        (s =>
+                            <div>
+                                {`${s.id || s.description} (Players count: ${s.playersCount})`}
+                                <Button onClick={() => join(s.id)}>Join</Button>
+                                <Button>Create invite</Button>
+                                {joiningError && <span style={{ color: 'red' }}>{joiningError}</span>}
+                            </div>
+                        )
+                }
+            </div>
+        }
+
+        <div style={{ margin: '1em' }}>
+            <Button onClick={toggleCreatePanelVisibility}>
+                {!creationFormVisible ? 'Create new session' : 'Hide'}
+            </Button>
+
+            {
+                creationFormVisible &&
+                <div>
+                    <input type='text' value={newSessionDescription} onChange={e => setNewSessionDescription(e.target.value)}></input>
+                    <Button style={{ margin: '0 1em 0' }} onClick={createNew}>
+                        Create
+                    </Button>
+                </div>
+            }
+        </div>
+    </>
+}
